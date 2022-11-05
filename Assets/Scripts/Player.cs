da78025eda566ccd3ab5b82fac9100b2aaf406c6
Alinda <Alindah@users.noreply.github.com>
2022-11-05 00:57:21 -0400

    Changed player speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,27 +10,32 @@ public class Player : Entity
 
     private void MovePlayer()
     {
-
-        /* 1. Create a function that makes the player move upwards,
-         * stopping when they've reached the edge.
+        /* https://docs.unity3d.com/Packages/com.unity.inputsystem@1.4/api/UnityEngine.InputSystem.html
          * 
-         * 2. Make it so that the player only moves up when the player presses a button.
+         * 1. Write code that will make the player move upwards.
          * 
-         * 3. Make the player move down.
+         * 2. Make player stop moving when they've reached the edge.
+         * 
+         * 3. Make it so that the player only moves up when the player presses a button.
+         * 
+         * 4. Make the player move down.
          */
 
+        // Set the Keyboard object as the current connected keyboard device
         Keyboard kb = Keyboard.current;
 
         // No keyboard connected
         if (kb == null)
             return;
 
+        // Move up
         if (kb.upArrowKey.IsPressed() || kb.wKey.IsPressed())
         {
             if (transform.position.y < yBoundaryUp)
                 transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
         }
 
+        // Move down
         if (kb.downArrowKey.IsPressed() || kb.sKey.IsPressed())
         {
             if (transform.position.y > yBoundaryDown)
